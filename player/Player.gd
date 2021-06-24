b3ea83bace8eafbe313b33abf39a72e6daab8995
unknown <kkeichi@gmail.com>
2021-06-24 17:37:02 -0300

    P_skins_mechanics

--- player/Player.gd
+++ player/Player.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 
+enum {JUMP,RUNNING}
+var status = RUNNING
 
 const SPEED = 500
 const GRAVITY = 10
@@ -12,17 +14,15 @@ export(int,"Player1","Player2") var Player
 
 func _ready():
 	yield(get_tree(), "idle_frame")
-	print(Player)
 
 
 func _physics_process(delta):
 	jump()
-	#move()
 	runner()
 	move_and_slide(motion,UP)
 	apply_gravity()
+	anim_status()
 	
-		
 	
 func apply_gravity():
 	if UP == Vector2(0,-1):
@@ -44,20 +44,21 @@ func apply_gravity():
 func jump():
 	if Player == 0:
 		if Input.is_action_pressed("jump") and is_on_floor():
-			$anim.play("jump")
+			status = JUMP
 			if UP == Vector2(0,-1):
 				motion.y -= JUMP_SPEED
 			elif UP == Vector2(0, 1):
 				motion.y += JUMP_SPEED
 	if Player == 1:
 		if Input.is_action_pressed("jump2") and is_on_floor():
-			$anim.play("jump")
+			status = JUMP
 			if UP == Vector2(0,-1):
 				motion.y -= JUMP_SPEED
 			elif UP == Vector2(0, 1):
 				motion.y += JUMP_SPEED
 
 func move():
+	status = RUNNING
 	if Input.is_action_pressed('ui_left') and not Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed('ui_right') and not Input.is_action_pressed("left"):
@@ -67,6 +68,22 @@ func move():
 
 
 func runner():
-	$anim.play("walking")
 	motion.x = SPEED
 
+func anim_status():
+	if is_on_floor():
+		status = RUNNING
+	elif motion.y < -20:
+		status = JUMP
+		
+	if Player == 0:
+		if status == RUNNING:
+			$anim.play("walking")
+		elif status == JUMP:
+			$anim.play("jump")
+			
+	elif Player == 1:
+		if status == RUNNING:
+			$anim2.play("walking")
+		elif status == JUMP:
+			$anim2.play("jump")
